42454159ccdf82d8b672a4251d53ee4819baff6b
BekDzyudo <jalilovshohbek9909@gmail.com>
2025-05-08 18:20:35 +0500

    fix

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -6,26 +6,55 @@ import { useGetFetch } from "../hooks/useGetFetch";
 
 function Quiz() {
   const { isTheme } = useContext(GlobalContext);
-  const [answers, setAnswers] = useState({});
+  const [answers, setAnswers] = useState(JSON.parse(localStorage.getItem("answers")) || {});
   const [selectOption, setSelectOption] = useState(
     JSON.parse(localStorage.getItem("selectOption")) || []
   );
+  const questionRefs = useRef([]);
+  
+// click and scroll
+  const handleScrollToQuestion = (index) => {
+    questionRefs.current[index]?.scrollIntoView({
+      behavior: "smooth",
+      block: "center"
+    });
+  };
 
-  const handleAnswerChange = (question_number, selectedOption) => {
+  // change input
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  useEffect(() => {
+    const savedAnswers = localStorage.getItem("saved_answers");
+    if (savedAnswers) {
+      setSelectedAnswers(JSON.parse(savedAnswers));
+    }
+  }, []);
+
+  const handleAnswerChange = (question_number, selectedOption, index1, index) => {
+    const update = {...selectedAnswers, [index1]: index}
+    setSelectedAnswers(update)
+    localStorage.setItem('saved_answers', JSON.stringify(update));
+
+    // =====
     setAnswers((prev) => ({
       ...prev,
       [question_number]: selectedOption,
     }));
-    setSelectOption(Object.keys(answers));
-    localStorage.setItem("selectOption", JSON.stringify(Object.keys(answers)));
+    
+    setSelectOption(Object.keys({...answers, [question_number]: selectedOption,}));
+    localStorage.setItem("selectOption", JSON.stringify(Object.keys({...answers, [question_number]: selectedOption,})));
+    localStorage.setItem("answers", JSON.stringify({...answers, [question_number]: selectedOption,}));
   };
 
+// submit
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("User answers:", answers);
     localStorage.removeItem("selectOption");
+    localStorage.removeItem("answers");
+    localStorage.removeItem("saved_answers");
   };
 
+  // get data
   const {
     data: quizzes,
     isPending,
@@ -33,7 +62,7 @@ function Quiz() {
   } = useGetFetch("http://localhost:3000/quizzes");
 
   // time ==================================================================
-  const defaultTime = 2 * 60 * 60 * 1000; // 2 soat msda
+  const defaultTime = 2 * 60 * 60 * 1000;
   const [remainingTime, setRemainingTime] = useState(() => {
     const saved = localStorage.getItem("remainingTime");
     return saved ? parseInt(saved) : defaultTime;
@@ -41,22 +70,22 @@ function Quiz() {
 
   const timerRef = useRef(null);
 
-  useEffect(() => {
-    timerRef.current = setInterval(() => {
-      setRemainingTime((prev) => {
-        const newTime = prev - 1000;
-        if (newTime <= 0) {
-          clearInterval(timerRef.current);
-          localStorage.removeItem("remainingTime");
-          return 0;
-        }
-        localStorage.setItem("remainingTime", newTime);
-        return newTime;
-      });
-    }, 1000);
+  // useEffect(() => {
+  //   timerRef.current = setInterval(() => {
+  //     setRemainingTime((prev) => {
+  //       const newTime = prev - 1000;
+  //       if (newTime <= 0) {
+  //         clearInterval(timerRef.current);
+  //         localStorage.removeItem("remainingTime");
+  //         return 0;
+  //       }
+  //       localStorage.setItem("remainingTime", newTime);
+  //       return newTime;
+  //     });
+  //   }, 1000);
 
-    return () => clearInterval(timerRef.current);
-  }, []);
+  //   return () => clearInterval(timerRef.current);
+  // }, []);
 
   const hours = Math.floor(remainingTime / (1000 * 60 * 60));
   const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
@@ -68,134 +97,149 @@ function Quiz() {
     <div className="min-h-screen pb-12">
       <Navbar />
       <div className="container flex gap-10 items-start justify-between">
-        <form className="w-[70%]" onSubmit={handleSubmit}>
-          <div className="quiz steps steps-vertical">
-            {isPending && <p>loading...</p>}
-            {error && <p>{error}</p>}
-            {quizzes &&
-              quizzes[2]?.questions.map((item, index1) => {
-                return (
-                  <div key={index1} className="step step-info text-lg mb-10">
-                    <div className="flex items-start gap-4 w-full">
-                      {/* Step aylanasi */}
-                      <div className="mt-2 w-6 flex-shrink-0 text-2xl"></div>
-                      <div className="flex flex-col gap-4 w-full">
-                        <h1 className="text-2xl text-start font-semibold border-b border-gray-400">
-                          {item.question}
-                        </h1>
-                        <div className="space-y-3 ml-6">
-                          {item.options.map((variant, index) => {
-                            return (
-                              <label
-                                key={index}
-                                className={`test-label group flex items-center gap-4 p-4 cursor-pointer ${
-                                  isTheme == "dracula"
-                                    ? "bg-[#3b4d66]"
-                                    : "bg-white"
-                                } rounded-lg`}
-                              >
-                                <div className="test-letter text-xl font-bold bg-gray-300 px-3 py-1 rounded group-hover:text-[#00A4F2] text-gray-500">
-                                  {String.fromCharCode(index + 65)}
-                                </div>
-                                <input
-                                  type="radio"
-                                  name={item.question}
-                                  onChange={() =>
-                                    handleAnswerChange(
-                                      index1 + 1,
-                                      String.fromCharCode(index + 65)
-                                    )
-                                  }
-                                />
-                                <div className="answerText text-xl text-start font-normal">
-                                  {variant}
-                                </div>
-                              </label>
-                            );
-                          })}
+        {isPending && <p>loading...</p>}
+        {error && <p>{error}</p>}
+        {quizzes && (
+          <>
+            <form className="w-[70%]" onSubmit={handleSubmit}>
+              <div className="quiz steps steps-vertical">
+                {
+                  quizzes[2]?.questions.map((item, index1) => {
+                    return (
+                      <div
+                        ref={(el) => (questionRefs.current[index1] = el)}
+                        key={index1}
+                        className="step step-info text-lg mb-10"
+                      >
+                        <div className="flex items-start gap-4 w-full">
+                          <div className="mt-2 w-6 flex-shrink-0 text-2xl"></div>
+                          <div className="flex flex-col gap-4 w-full">
+                            <h1 className="text-2xl text-start font-semibold border-b border-gray-400">
+                              {item.question}
+                            </h1>
+                            <div className="space-y-3 ml-6">
+                              {item.options.map((variant, index) => {
+                                 const isSelected = selectedAnswers[index1] === index;
+                                return (
+                                  <label
+                                  style={{border:"3px solid", borderColor: isSelected ? "#00A4F2" : "transparent"}}
+                                    key={index}
+                                    className={`test-label group flex items-center gap-4 p-4 cursor-pointer ${
+                                      isTheme == "dracula"
+                                        ? "bg-[#3b4d66]"
+                                        : "bg-white"
+                                    } rounded-lg`}
+                                  >
+                                    <div className="test-letter text-xl font-bold bg-gray-300 px-3 py-1 rounded group-hover:text-[#00A4F2] text-gray-500">
+                                      {String.fromCharCode(index + 65)}
+                                    </div>
+                                    <input
+                                      type="radio"
+                                      name={item.question}
+                                      onChange={() =>
+                                        handleAnswerChange(
+                                          index1 + 1,
+                                          String.fromCharCode(index + 65),
+                                          index1,
+                                          index
+                                        )
+                                      }
+                                    />
+                                    <div className="answerText text-xl text-start font-normal">
+                                      {variant}
+                                    </div>
+                                  </label>
+                                );
+                              })}
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                );
-              })}
-          </div>
-          <div className="w-full flex flex-col items-center">
-            <button
-              type="submit"
-              className="w-1/2 btn btn-info btn-xl text-white"
-            >
-              Testni yakunlash
-            </button>
-          </div>
-        </form>
-        <div className="sidebar w-[30%] p-5 border border-gray-400 sticky top-32 rounded-xl">
-          <div className="user flex items-center gap-5 border-b border-gray-400 pb-1">
-            <FaUser style={{ color: "gray", fontSize: "25px" }} />{" "}
-            <h1 className="text-center text-2xl font-semibold">
-              Alimardonov Valijon
-            </h1>
-          </div>
-          {/* time */}
-          <div className="flex justify-center m-5">
-            <span className="countdown font-mono text-3xl">
-              <span
-                style={{ "--value": hours }}
-                aria-live="polite"
-                aria-label={counter}
-              >
-                {hours}
-              </span>
-              :
-              <span
-                style={{ "--value": minutes }}
-                aria-live="polite"
-                aria-label={counter}
-              >
-                {minutes}
-              </span>
-              :
-              <span
-                style={{ "--value": seconds }}
-                aria-live="polite"
-                aria-label={counter}
-              >
-                {seconds}
-              </span>
-            </span>
-          </div>
-          {/* process */}
-          <div className="my-3 mb-10">
-            <h1>Process</h1>
-            <div className="test-proccess-container">
-              {quizzes && (
-                <div
-                  className="test-proccess"
-                  style={{
-                    width:
-                      (selectOption.length / quizzes[2]?.questions?.length) *
-                        100 +
-                      "%",
-                  }}
-                ></div>
-              )}
-            </div>
-          </div>
-          {/* btns */}
-          <div className="flex gap-[5px] flex-wrap">
-            {quizzes &&
-              quizzes[2]?.questions.map((item, index) => {
-                return (
-                  <button
-                    key={index}
-                    className="btn btn-outline text-[16px] mb-2"
+                    );
+                  })}
+              </div>
+              <div className="w-full flex flex-col items-center">
+                <button
+                  type="submit"
+                  className="w-1/2 btn btn-info btn-xl text-white"
+                >
+                  Testni yakunlash
+                </button>
+              </div>
+            </form>
+            <div className="sidebar w-[30%] p-5 border border-gray-400 sticky top-32 rounded-xl">
+              <div className="user flex items-center gap-5 border-b border-gray-400 pb-1">
+                <FaUser style={{ color: "gray", fontSize: "25px" }} />{" "}
+                <h1 className="text-center text-2xl font-semibold">
+                  Alimardonov Valijon
+                </h1>
+              </div>
+              {/* time */}
+              <div className="flex justify-center m-5">
+                <span className="countdown font-mono text-3xl">
+                  <span
+                    style={{ "--value": hours }}
+                    aria-live="polite"
+                    aria-label={counter}
                   >
-                    {index + 1}
-                  </button>
-                );
-              })}
-          </div>
-        </div>
+                    {hours}
+                  </span>
+                  :
+                  <span
+                    style={{ "--value": minutes }}
+                    aria-live="polite"
+                    aria-label={counter}
+                  >
+                    {minutes}
+                  </span>
+                  :
+                  <span
+                    style={{ "--value": seconds }}
+                    aria-live="polite"
+                    aria-label={counter}
+                  >
+                    {seconds}
+                  </span>
+                </span>
+              </div>
+              {/* process */}
+              <div className="my-3 mb-10">
+                <h1>Process</h1>
+                <div className="test-proccess-container">
+                  {quizzes && (
+                    <div
+                      className="test-proccess"
+                      style={{
+                        width:
+                          (selectOption.length /
+                            quizzes[2]?.questions?.length) *
+                            100 +
+                          "%",
+                      }}
+                    ></div>
+                  )}
+                </div>
+              </div>
+              {/* btns */}
+              <div className="flex gap-[5px] flex-wrap">
+                {quizzes &&
+                  quizzes[2]?.questions.map((item, index) => {  
+                    const isChanged = selectOption.includes(`${index + 1}`);   
+                          
+                    return (
+                      <button
+                        onClick={() => handleScrollToQuestion(index)}
+                        key={index}
+                        className={`btn ${isChanged ? "btn-info" : "btn-outline"} text-[16px] mb-2`}
+                      >
+                        {index + 1}
+                      </button>
+                    )
+                  })}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
